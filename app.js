1ea860366e431c3b8686c66b33fedc91e427064e
John Bartmann <pravda23@gmail.com>
2022-04-19 09:04:28 +0200

    js frame sliders

---
+++ app.js
@@ -0,0 +1,12 @@
+// selectors 
+const left = document.querySelector('#left')
+
+// event listeners
+left.addEventListener('click', leftReset)
+
+// functions
+function leftReset(e) {
+    e.target.style.backgroundColor = 'red'
+    e.target.style.color = -71 %
+        console.log(e)
+}
