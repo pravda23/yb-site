cf99ad29e803e8d19903c88978b932d0d97d1281
John Bartmann <pravda23@gmail.com>
2022-04-20 11:28:00 +0200

    attempting state object stuff

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const arr = ['left', 'right', 'top', 'bottom']
+const state = {
+    top: 'off',
+    right: 'off',
+    bottom: 'off',
+    left: 'off',
+}
 
 // selectors 
 const top01 = document.querySelector('.top')
@@ -9,18 +14,41 @@ const leftOverlay = document.querySelector('.leftOverlay')
 const rightOverlay = document.querySelector('.rightOverlay')
 const topOverlay = document.querySelector('.topOverlay')
 const bottomOverlay = document.querySelector('.bottomOverlay')
+const arrows = document.querySelector('i')
 
 // // event listeners
 
+arrows.style.backgroundColor = 'red'
 this.addEventListener('click', (i) => {
-    console.log()
-    for (j in arr) {
-        if (i.target.classList[0] === arr[j]) {
 
-            i.target.classList.toggle(`${i.target.classList[0]}Overlay`)
+
+    function resetState() {
+        Object.keys(state).forEach(key => {
+            state[key] = 'off';
+        })
+    }
+
+    for (let j = 0; j <= 4; j++) {
+        resetState()
+        if (i.target.classList[0] === Object.keys(state)[j]) {
+            // console.log(i.target.classList[0])
+            // console.log(Object.values(state))
+            // let objKeys = (Object.keys(state))
+            // console.log(objKeys[1])
+            // console.log(Object.keys(state)[j])
+
+            state[i.target.classList[0]] = 'on'
+            console.log(state)
         }
 
+        Object.keys(state).forEach(value => {
+            if (state[value] === 'off') {
+                i.target.classList.toggle(`${i.target.classList[0]}Overlay`)
+            }
+            console.log(state[value])
+        })
     }
+
 })
 
 // this.addEventListener('click', (i) => {
