68dba7912675427e22957e8dffcb30cdee564778
John Bartmann <pravda23@gmail.com>
2022-05-04 14:51:34 +0300

    panel content in progress

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 // selectors 
-const top01 = document.querySelector('.top')
+
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
-const bottom = document.querySelector('.bottom')
-const leftOverlay = document.querySelector('.leftOverlay')
-const rightOverlay = document.querySelector('.rightOverlay')
-const topOverlay = document.querySelector('.topOverlay')
-const bottomOverlay = document.querySelector('.bottomOverlay')
-const arrows = document.querySelector('i')
+const leftArrow = document.querySelector('.fa-circle-left')
+const rightArrow = document.querySelector('.fa-circle-right')
+const leftText = document.querySelector('.left-text')
 
-// when user clicks a panel, the panel is identified and its status in the global state object is updated to "on"
-
-top01.addEventListener('click', (event) => {
-    event.target.classList.toggle('overlayUp')
-})
+// event listeners
+// console.log(arrows.classList)
 
 right.addEventListener('click', (event) => {
+    left.classList.remove('overlayUp')
     event.target.classList.toggle('overlayUp')
-})
+    leftArrow.classList.toggle('flipArrow')
 
-bottom.addEventListener('click', (event) => {
-    event.target.classList.toggle('overlayUp')
 })
 
 left.addEventListener('click', (event) => {
+    right.classList.remove('overlayUp')
     event.target.classList.toggle('overlayUp')
+    rightArrow.classList.toggle('flipArrow')
+    leftText.class
 })
